936bbcc801693c7da409b9e5c7c90dbefe95ac7f
tanujsonare <tanujsonare1910@gmail.com>
2023-05-15 15:07:02 +0530

    Updates

---
+++ precache-manifest.3b4a3c3a0fb2d1ff948226809314a727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42985399c399a5be30fe",
+    "url": "/TextReactapp/static/css/main.b697541b.chunk.css"
+  },
+  {
+    "revision": "42985399c399a5be30fe",
+    "url": "/TextReactapp/static/js/main.42985399.chunk.js"
+  },
+  {
+    "revision": "d3df32626652fce886c3",
+    "url": "/TextReactapp/static/js/1.d3df3262.chunk.js"
+  },
+  {
+    "revision": "3c016175441de394fef9",
+    "url": "/TextReactapp/static/js/2.3c016175.chunk.js"
+  },
+  {
+    "revision": "6c5d930677629170fad0",
+    "url": "/TextReactapp/static/js/runtime~main.6c5d9306.js"
+  },
+  {
+    "revision": "54cb87c5cf149d1a570a9fb7e0c173be",
+    "url": "/TextReactapp/index.html"
+  }
+];
